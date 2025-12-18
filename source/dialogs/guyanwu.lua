9d69f6542d0243bde603bdc2c063f88ea07912d7
mogita <me@mogita.com>
2025-12-18 18:38:08 +0800

    feat: add demo dialogs

---
+++ source/dialogs/guyanwu.lua
@@ -0,0 +1,18 @@
+-- Dialog for 顾炎武 (Gu Yanwu) - The scholarly master
+-- A learned scholar who takes everything very seriously
+
+return {
+	{
+		speaker = "顾炎武",
+		text = "你来得正好！我刚刚发现了一个惊天秘密——原来糖葫芦的糖是甜的！这将改变整个武林的格局！"
+	},
+	{
+		speaker = "顾炎武",
+		text = "等等，让我先记录下来。笔呢？我的笔又被小书童拿去换糖葫芦了……"
+	},
+	{
+		speaker = "顾炎武",
+		text = "唉，算了。你知道吗？我昨天研究了一整夜的武功秘籍，结果发现那是一本菜谱。红烧狮子头的做法倒是挺详细的。"
+	}
+}
+
---
+++ source/dialogs/xiaoshutong.lua
@@ -0,0 +1,22 @@
+-- Dialog for 小书童 (Xiao Shutong) - The mischievous book boy
+-- Always hungry and obsessed with tanghulu (candied hawthorn)
+
+return {
+	{
+		speaker = "小书童",
+		text = "嘿嘿，你有糖葫芦吗？我可以用师父的珍藏秘籍跟你换！"
+	},
+	{
+		speaker = "小书童",
+		text = "什么？你说这样不对？放心，师父根本不会发现的。上次我把他的《九阳真经》换了三串糖葫芦，他到现在还以为是被老鼠啃了。"
+	},
+	{
+		speaker = "小书童",
+		text = "对了，你见到何铁手了吗？他欠我五串糖葫芦！上次比武输给我，说好请我吃的，结果到现在还装作不记得。"
+	},
+	{
+		speaker = "小书童",
+		text = "唉，师父又让我抄书了。你知道吗？我已经把《论语》抄了八百遍了。现在我闭着眼睛都能倒背如流……倒着背还押韵呢！"
+	}
+}
+
