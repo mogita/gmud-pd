146f67c16cc038b8199f73e1f0c3b35cbe5e3060
mogita <me@mogita.com>
2025-12-18 18:17:08 +0800

    feat: use default settings

--- source/main.lua
+++ source/main.lua
@@ -23,13 +23,10 @@ local dialog = DialogBox.new({
 			text = "顾炎武今日不在武馆，请给我糖葫芦，我会将毛笔给你。若想要切磋，请与旁边的何铁手交谈。若无其他要事，我就去忙了。刚才我们在函数详解这说了四个点，现在我们来说明第五个点。我定义一个变量 num 1，接下来我调用一个方法，把 10 和 20 传进去。",
 		},
 		{
-			speaker = "顾炎武",
+			speaker = "顾炎武的大毛猫咪",
 			text = "原来如此，多谢告知。",
 		},
-	},
-	speed = "fast", -- "instant", "fast", "slow"
-	fontSize = "medium", -- "small" (12px), "medium" (16px), "large" (18px)
-	invert = false,
+	}
 })
 
 -- Add dialog sprite to display list
